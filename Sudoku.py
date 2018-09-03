24e4df36538a719516b49e282d823492fab4efeb
Levi J. Masters <42942430+Levijom@users.noreply.github.com>
2018-09-03 14:47:30 -0400

    Add files via upload

---
+++ Sudoku.py
@@ -0,0 +1,33 @@
+
+# vertical rows
+rowA = [Aa,Ab,Ac,Ad,Ae,Af,Ag,Ah,Ai]
+rowB = [Ba,Bb,Bc,Bd,Be,Bf,Bg,Bh,Bi]
+rowC = [Ca,Cb,Cc,Cd,Ce,Cf,Cg,Ch,Ci]
+rowD = [Da,Db,Dc,Dd,De,Df,Dg,Dh,Di]
+rowE = [Ea,Eb,Ec,Ed,Ee,Ef,Eg,Eh,Ei]
+rowF = [Fa,Fb,Fc,Fd,Fe,Ff,Fg,Fh,Fi]
+rowG = [Ga,Gb,Gc,Gd,Ge,Gf,Gg,Gh,Gi]
+rowH = [Ha,Hb,Hc,Hd,He,Hf,Hg,Hh,Hi]
+rowI = [Ia,Ib,Ic,Id,Ie,If,Ig,Ih,Ii]
+
+#Horizontal
+rowa = [Aa,Ba,Ca,Da,Ea,Fa,Ga,Ha,Ia]
+rowb = [Ab,Bb,Cb,Db,Eb,Fb,Gb,Hb,Ib]
+rowc = [Ac,Bc,Cc,Dc,Ec,Fc,Gc,Hc,Ic]
+rowd = [Ad,Bd,Cd,Dd,Ed,Fd,Gd,Hd,Id]
+rowe = [Ae,Be,Ce,De,Ee,Fe,Ge,He,Ie]
+rowf = [Af,Bf,Cf,Df,Ef,Ff,Gf,Hf,If]
+rowg = [Ag,Bg,Cg,Dg,Eg,Fg,Gg,Hg,Ig]
+rowh = [Ah,Bh,Ch,Dh,Eh,Fh,Gh,Hh,Ih]
+rowi = [Ai,Bi,Ci,Di,Ei,Fi,Gi,Hi,Ii]
+
+#Boxes
+boxAa = [Aa,Ab,Ac,Ba,Bb,Bc,Ca,Cb,Cc]
+boxAb = [Ad,Ae,Af,Bd,Be,Bf,Cd,Ce,Cf]
+boxAc = [Ag,Ah,Ai,Bg,Bh,Bi,Cg,Ch,Ci]
+boxBa = [Da,Db,Dc,Ea,Eb,Ec,Fa,Fb,Fc]
+boxBb = [Dd,De,Df,Ed,Ee,Ef,Fd,Fe,Ff]
+boxBc = [Dg,Dh,Di,Eg,Eh,Ei,Fg,Fh,Fi]
+boxCa = [Ga,Gb,Gc,Ha,Hb,Hc,Ia,Ib,Ic]
+boxCb = [Gd,Ge,Gf,Hd,He,Hf,Id,Ie,If]
+boxCc = [Gg,Gh,Gi,Hg,Hh,Hi,Ig,Ih,Ii]
